23268a5dd1a04f04b61b342e7658a7af2e264a38
Kreee <kreeejiang@outlook.com>
2021-11-16 16:18:15 +0800

    Finish basic structure

---
+++ site-config.js
@@ -0,0 +1,25 @@
+export const siteconf = {
+    main: {
+        avatar: 'https://avatars.githubusercontent.com/u/76862878',
+        description: 'Code with love ❤️ and paws 🐾!',
+        buttons: [
+            {name:'GitHub', fa:'fa-brands fa-github', url:'https://github.com/ohmykreee'}, 
+            {name:'Twitter', fa:'fa-brands fa-twitter', url:'https://twitter.com/kreeejiang'},
+            {name:'Steam', fa:'fa-brands fa-steam', url:'https://steamcommunity.com/id/kreeejiang/'},
+            {name:'Bilibili', fa:'fa-brands fa-bilibili', url:'https://space.bilibili.com/361507684'}],
+    },
+
+    kblog: {
+        avatar: 'https://blog.ohmykreee.top/android-chrome-256x256.png',
+        description: 'Just a blog, blogging my life.',
+        buttons: [],
+    },
+
+    eureka: {
+        avatar: 'https://blog.ohmykreee.top/android-chrome-256x256.png',
+        description: 'This is eureka, for testing purpose.',
+        buttons: [],
+    },
+
+    footer: 'Copyright © 2021 Kreee Jiang.',
+}
