1637973a5d527186f50668e737a7563bb8763bf9
Kreee <contact@ohmykreee.top>
2021-11-17 18:23:15 +0800

    Ready to publish!

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import React from 'react'
 import styles from '../styles/Home.module.css'
 import { siteconf } from '../site-config.js'
@@ -13,8 +12,29 @@ export default function Home() {
     <div className={styles.container}>
     <Head>
       <title>Home Page - Kreee</title>
-      <meta name="description" content="Home page for Kreee" />
-      <link rel="icon" href="/favicon.ico" />
+
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="author" content="Kreee" />
+      <meta name="description" content="Home page of ohmykreee.top" />
+      <meta name="keywords" content="kreee,personal,responsive,font awesome,react" />
+      <meta name="title" content="Home Page - Kreee" />
+      <meta itemProp="name" content="Home Page - Kreee" />
+      <meta itemProp="description" content="Home page of ohmykreee.top" />
+      <meta property="og:title" content="Home Page - Kreee" />
+      <meta property="og:description" content="Home page of ohmykreee.top" />
+      <meta property="og:image" content="/avatar.jpg" />
+      <meta property="og:url" content="https://www.ohmykreee.top/" />
+      <meta property="og:site_name" content="Home Page - Kreee" />
+      <meta property="og:type" content="website" />
+
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#603cba" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
 
       <main className={styles.main}>
@@ -96,7 +116,7 @@ class Buttonlist extends React.Component {
   render() {
     const lists = this.props.buttons
     const renderButton = lists.map((item) => 
-      <a key={item.name} href={item.url} target="_blank" title={item.name}>
+      <a key={item.name} href={item.url} target="_blank" title={item.name} rel="noreferrer">
         <FontAwesomeIcon icon={item.fa} color="white"></FontAwesomeIcon>
       </a>
     )
