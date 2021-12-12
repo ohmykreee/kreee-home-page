a877c25ae91cd6399950bc2df657ca51688b9df5
Kreee <yangke.jiang@outlook.com>
2021-12-12 21:42:15 +0800

    Chore: fix typo

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,5 @@
-import { Header } from '../compoents/Header'
-import { Footer } from '../compoents/Footer'
+import { Header } from '../components/Header'
+import { Footer } from '../components/Footer'
 import React from 'react'
 import styles from '../styles/Home.module.css'
 import { siteconf } from '../site-config.js'
